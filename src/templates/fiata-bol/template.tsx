58f40c8064389f3a2fa9f41a2e780268f286131a
rajkishorepatra <rpatrasm@gmail.com>
2024-08-30 14:47:04 +0530

    minor changes

--- src/templates/fiata-bol/template.tsx
+++ src/templates/fiata-bol/template.tsx
@@ -38,7 +38,7 @@ export const FiataEBolTemplate: FunctionComponent<TemplateProps<FiataEBLData>> =
   `;
 
   return (
-    <div style={{ backgroundColor: "#7acbd8", width:"80%", display:"flex", justifyContent:"center", alignItems:"center", margin:"auto" }}>
+    <div style={{ backgroundColor: "#7acbd8", display:"flex", justifyContent:"center", alignItems:"center", margin:"auto" }}>
       <div css={containerStyle}>
         <table
           style={{
