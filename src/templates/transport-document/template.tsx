7814c4c98b31c54902ef0285fca08c0cf28e4b9d
abhilashsethi <abhilashsethi9@gmail.com>
2026-08-14 12:38:21 +0530

    Fix: blockchain name fixed

--- src/templates/transport-document/template.tsx
+++ src/templates/transport-document/template.tsx
@@ -664,7 +664,7 @@ export const CargoDocumentTemplate: FunctionComponent<TemplateProps<CargoDocumen
           </tr> */}
         <div css={tableTd} style={{ display: "flex", justifyContent: "space-between", fontSize: "0.8rem" }}>
           <div>
-            <b>Blockchain:</b>&nbsp;{blockchainName === "xinfin" ? "XDC Network" : "polygon"}
+            <b>Blockchain:</b>&nbsp;{blockchainName === "xinfin" ? "XDC Network" : "XDC Network"}
           </div>
           {/* <div>
             <b>Genesis Transaction Hash:</b>&nbsp;{txHash?.split("/").pop()}
