db3644af64ac6676618b2fa3c98b5e7d68482e1f
Swaroop Sahoo <solutionsutras@gmail.com>
2026-09-10 17:06:41 +0530

    Terms n Conditions changes

--- src/templates/samples/bulkBOLTemplate.ts
+++ src/templates/samples/bulkBOLTemplate.ts
@@ -26,7 +26,7 @@ export const bulkEblTemplate: BulkBOLData = {
           organisationId: "34"
         }
       },
-      carrierLogo: "https://ccigroup.co.in/wp-content/uploads/2023/09/CCI-Group-2.png",
+      carrierLogo: "",
       vesselName: "vessel",
       dateOfIssue: "2026-09-23T00:00:00.000Z",
       measurement: "20",
