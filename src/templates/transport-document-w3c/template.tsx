15c898da44b1c63aad159b5392c7a4439c275ea5
abhilashsethi <abhilashsethi9@gmail.com>
2026-09-17 17:39:26 +0530

    changes

--- src/templates/transport-document-w3c/template.tsx
+++ src/templates/transport-document-w3c/template.tsx
@@ -4,9 +4,22 @@ import { CargoDocumentTemplate } from "../transport-document/template";
 import { adaptW3CDocument } from "./w3cAdapter";
 import { W3CCargoDocument } from "./types";
 
-export const W3CCargoDocumentTemplate = (props: TemplateProps<any>) => {
+export const W3CCargoDocumentTemplate = (
+  props: TemplateProps<any>
+) => {
+  console.log(
+    "🔥 W3C CARGO DOCUMENT TEMPLATE LOADED",
+    props.document
+  );
+
   const w3cDocument = props.document as W3CCargoDocument;
+
   const cargoDocument = adaptW3CDocument(w3cDocument);
 
-  return <CargoDocumentTemplate document={cargoDocument} handleObfuscation={props.handleObfuscation} />;
+  return (
+    <CargoDocumentTemplate
+      document={cargoDocument}
+      handleObfuscation={props.handleObfuscation}
+    />
+  );
 };
