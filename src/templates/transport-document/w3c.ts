79b27a215d290f0d4657091ffbe5baba8eb74eea
abhilashsethi <abhilashsethi9@gmail.com>
2026-09-17 15:42:16 +0530

    changes

--- src/templates/transport-document/w3c.ts
+++ src/templates/transport-document/w3c.ts
@@ -2,8 +2,8 @@ import { W3CCargoDocumentTemplate } from "./w3cTemplate";
 
 export const W3CCargoDocumentTemplates = [
   {
-    id: "cargoDocumentTemplate",
-    label: "cargoDocumentTemplate",
+    id: "DCSA_BOL_W3C",
+    label: "DCSA_BOL_W3C",
     template: W3CCargoDocumentTemplate,
   },
 ];
